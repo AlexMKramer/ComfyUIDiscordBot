201351dfce595f01957b6ae998bb1c18702daaad
AlexMKramer <93092632+AlexMKramer@users.noreply.github.com>
2023-08-16 08:52:39 -0600

    Add Lora Support and update README

--- main.py
+++ main.py
@@ -189,6 +189,8 @@ async def crazy(ctx):
     required=False
 )
 async def draw(ctx, new_prompt: str, new_style: str, new_height_width: str, new_upscale: str, new_lora: str):
+    if new_lora is not None:
+        new_prompt = " <lora:" + new_lora + ":0.5>," + new_prompt
     if new_style is not None and new_height_width is not None:
         await ctx.respond(
             f"Generating images for {ctx.author.mention}\n**Prompt:** {new_prompt}\n**Style:** {new_style}\n**Height/Width:** {new_height_width}")
@@ -200,8 +202,6 @@ async def draw(ctx, new_prompt: str, new_style: str, new_height_width: str, new_
             f"Generating images for {ctx.author.mention}\n**Prompt:** {new_prompt}\n**Height/Width:** {new_height_width}")
     else:
         await ctx.respond(f"Generating images for {ctx.author.mention}\n**Prompt:** {new_prompt}")
-    if new_lora is not None:
-        new_prompt = " <lora:" + new_lora + ":0.5>," + new_prompt
     prompt["146"]["inputs"]["text_positive"] = new_prompt
     seed = random.randint(0, 0xffffffffff)
     prompt["22"]["inputs"]["noise_seed"] = int(seed)
