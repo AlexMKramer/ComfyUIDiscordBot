59116bdf9643bca74c55e7fb62bd2378fab2473b
AlexMKramer <93092632+AlexMKramer@users.noreply.github.com>
2023-08-19 13:46:45 -0600

    Add facefix, simplify upscale, add model support.

--- main.py
+++ main.py
@@ -127,8 +127,8 @@ async def models_autocomplete(ctx: discord.AutocompleteContext):
 
             # List files within the target subfolder
             subfolder_files = [file for file in os.listdir(subfolder_path)]
-            return sorted(
-                [os.path.splitext(models)[0] for models in subfolder_files])
+            matching_files = [models for models in subfolder_files if models.startswith(ctx.value.lower())]
+            return sorted(matching_files)
 
     # If the target subfolder is not found
     return []
