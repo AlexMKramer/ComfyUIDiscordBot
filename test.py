7d2bae749e9ec70b2b01e91f776ce7031ae23ad5
AlexMKramer <93092632+AlexMKramer@users.noreply.github.com>
2023-08-23 08:04:21 -0600

    Testing Queue system

--- test.py
+++ test.py
@@ -31,36 +31,6 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 bot.auto_sync_commands = True
 magic_instance = magic.Magic()
 
-request_queue = asyncio.Queue()
-
-
-async def queue_position(ctx):
-    position = request_queue.qsize()  # Get the current queue size (position in the queue)
-    if position == 0:
-        await ctx.send(f"Generating image for {ctx.author.mention}")
-        return
-    await ctx.send(f"You are in position {position} in the queue.")
-
-
-async def process_requests():
-    while True:
-        ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image = await request_queue.get()  # Dequeue the next request
-        await process_request(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image)
-        request_queue.task_done()  # Mark the request as done
-
-
-async def process_request(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image):
-    try:
-        message = await form_message(ctx.author.mention, new_prompt, new_negative, new_style, new_size, new_lora, model_name)
-        if includes_image:
-            file_list = await generate_img2img(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image)
-        else:
-            file_list = await generate_image(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image)
-        await ctx.send(message, files=file_list)
-    except Exception as e:
-        print(e)
-        await ctx.send(ctx.author.mention + " Something went wrong. Please try again.")
-
 
 @bot.event
 async def on_connect():
@@ -68,56 +38,6 @@ async def on_connect():
         await bot.sync_commands()
     print(f'Logged in as {bot.user.name}')
 
-
-@bot.event
-async def on_ready():
-    bot.loop.create_task(process_requests())
-
-
-def gpt_integration(text):
-    gpt_new_prompt = ({"role": "user", "content": "Here are the lyrics I would like in this format:" + text})
-    gpt_message = gpt_initial_prompt + [gpt_new_prompt]
-    try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=gpt_message
-        )
-        reply_content = completion.choices[0].message.content
-        start_index = reply_content.find('{')
-        end_index = reply_content.rfind('}')
-
-        if start_index != -1 and end_index != -1 and end_index > start_index:
-            new_text = reply_content[start_index:end_index + 1]
-            print(f'ChatGPT reply: {new_text}')
-            return new_text
-        else:
-            print(f'ChatGPT reply: {reply_content}')
-            return reply_content
-    except Exception as e:
-        print(e)
-        return None
-
-
-gpt_initial_prompt = [{'role': 'user',
-                       'content': "Using song lyrics, come up with a prompt for an image generator.  "
-                                  "Please follow the format exactly. The format should be broken down "
-                                  "like this: {Art Style}, {Subject}, {Details}, {Color}\n The art style "
-                                  "should be determined by the overall impression of the song.  If it is "
-                                  "sad, then something like La Douleur should be used. If it is happy, "
-                                  "perhaps a vibrant street art style.\nThe Subject should be determined "
-                                  "by who the song is about.  If the song is about a couple trying to "
-                                  "escape the city, then the subject should be a couple.\nThe Details "
-                                  "should be determined by descriptive words used in the song.  If they "
-                                  "mention empty bottles, then add empty bottles to the prompt.\nThe "
-                                  "color should be determined by the mood of the song.  If the mood is a "
-                                  "happy one, use bright colors.\nHere is an example:\n{A dreamlike and "
-                                  "ethereal art style}, {a couple standing on a cliffside embracing, "
-                                  "overlooking a surreal and beautiful landscape}, {sunset, grassy, "
-                                  "soft wind}, {soft pastels, with hints of warm oranges and pinks}"},
-                      {'role': 'assistant',
-                       'content': "{Vibrant and energetic street art style}, {a group of friends dancing and "
-                                  "celebrating under the city lights}, {joyful, urban, rhythm}, {bold and lively "
-                                  "colors, with splashes of neon blues and pinks}"}, ]
 
 with open('prompts.json', 'r') as sdxl_prompts:
     prompts_data = json.load(sdxl_prompts)
@@ -190,36 +110,45 @@ async def models_autocomplete(ctx: discord.AutocompleteContext):
     return []
 
 
-def fix_lyrics(text):
-    keyword1 = "Lyrics"
-    keyword2 = r"\d*Embed|Embed"
-    start_index = text.find(keyword1)
-    end_index = re.search(keyword2, text[start_index])
+request_queue = asyncio.Queue()
+processing_lock = asyncio.Lock()
+
+
+async def queue_position(ctx):
+    position = request_queue.qsize()  # Get the current queue size (position in the queue)
+    if position == 0:
+        await ctx.send(f"Generating image for {ctx.author.mention}")
+        return
+    await ctx.send(f"You are in position {position} in the queue.")
+
+
+async def process_requests():
+    while True:
+        ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image = await request_queue.get()  # Dequeue the next request
+        async with processing_lock:  # Acquire the lock before processing
+            await process_request(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name,
+                                  includes_image)
+            request_queue.task_done()  # Mark the request as done
+
+
+async def process_request(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image):
     try:
-        if start_index != -1 and end_index:
-            lyrics_in_index = start_index + end_index.start()
-            text = text[start_index + len(keyword1):lyrics_in_index].strip()
+        message = await form_message(ctx.author.mention, new_prompt, new_negative, new_style, new_size, new_lora,
+                                     model_name)
+        if includes_image:
+            file_list = None
         else:
-            text = text
-        ad_pattern = r'See .*? LiveGet tickets as low as \$\d+You might also like'
-        re.sub(ad_pattern, '', text)
-        re.sub(r'\[.*?\]', '', text)
-        re.sub(r'\d+$', '', text)
-        re.sub('"', '', text)
-    except Exception as e:
-        print(f'Error: {e}\'\nLyrics: {text}')
-    return text
-
-
-def get_lyrics(song, artist):
-    try:
-        song = genius.search_song(song, artist)
-        new_lyrics = song.lyrics
-        fixed_lyrics = fix_lyrics(new_lyrics)
-        return fixed_lyrics
+            file_list = await generate_image(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name,
+                                             includes_image)
+        await ctx.send(message, files=file_list)
     except Exception as e:
         print(e)
-        return None
+        await ctx.send(ctx.author.mention + " Something went wrong. Please try again.")
+
+
+@bot.event
+async def on_ready():
+    bot.loop.create_task(process_requests())
 
 
 async def form_message(
@@ -295,55 +224,6 @@ async def generate_image(ctx, new_prompt, new_negative, new_style, new_size, new
         return file_list
 
 
-async def generate_img2img(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image):
-    if new_lora is not None:
-        new_prompt = " <lora:" + new_lora + ":0.5>, " + new_prompt
-    img2img_prompt["146"]["inputs"]["text_positive"] = new_prompt
-
-    if new_negative is not None:
-        img2img_prompt["146"]["inputs"]["text_negative"] = new_negative
-    else:
-        img2img_prompt["146"]["inputs"]["text_negative"] = ''
-
-    if new_style is not None:
-        if new_style == 'random':
-            new_style = random.choice(style_names)
-        img2img_prompt["146"]["inputs"]["style"] = new_style
-    else:
-        img2img_prompt["146"]["inputs"]["style"] = 'base'
-
-    if model_name is not None:
-        img2img_prompt["10"]["inputs"]["ckpt_name"] = model_name
-    else:
-        img2img_prompt["10"]["inputs"]["ckpt_name"] = base_model
-
-    if new_size is not None:
-        height, width = new_size.split()
-        img2img_prompt["160"]["inputs"]["height"] = int(height)
-        img2img_prompt["160"]["inputs"]["width"] = int(width)
-    else:
-        img2img_prompt["160"]["inputs"]["height"] = 1024
-        img2img_prompt["160"]["inputs"]["width"] = 1024
-
-    seed = random.randint(0, 0xffffffffff)
-    img2img_prompt["22"]["inputs"]["noise_seed"] = int(seed)
-
-    ws = websocket.WebSocket()
-    ws.connect("ws://{}/ws?clientId={}".format(comfyAPI.server_address, comfyAPI.client_id))
-    images = comfyAPI.get_images(ws, img2img_prompt)
-    file_paths = []
-    for node_id in images:
-        for image_data in images[node_id]:
-            image = Image.open(io.BytesIO(image_data))
-            with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as temp_file:
-                image.save(temp_file.name)
-                file_paths.append(temp_file.name)
-        file_list = [discord.File(file_path) for file_path in file_paths]
-        for file_path in file_paths:
-            os.remove(file_path)
-        return file_list
-
-
 @bot.slash_command(description='Generate images using only words!')
 @option(
     "new_prompt",
@@ -384,7 +264,9 @@ async def draw(ctx,
                ):
     includes_image = False
     await ctx.defer()
-    await request_queue.put((ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image))
+    async with processing_lock:  # Acquire the lock
+        await request_queue.put(
+            (ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image))
 
 
 @bot.slash_command(description='Go Crazy!')
@@ -409,185 +291,6 @@ async def crazy(ctx):
     except Exception as e:
         print(e)
         await ctx.send(ctx.author.mention + " Something went wrong. Please try again.")
-
-
-@bot.slash_command(description="Interpret a song's lyrics using ChatGPT!")
-@option(
-    "song",
-    description="Enter the song name",
-    required=True
-)
-@option(
-    "artist",
-    description="Enter the artist name",
-    required=True
-)
-@option(
-    "model_name",
-    description="Choose the model",
-    autocomplete=models_autocomplete,
-    required=False
-)
-async def interpret(ctx,
-                    song: str,
-                    artist: str,
-                    model_name: str = None
-                    ):
-    author_name = ctx.author.mention
-    await ctx.respond(f"Getting lyrics for {ctx.author.mention}\n**Song:** {song}\n**Artist:** {artist}")
-    fixed_lyrics = get_lyrics(song, artist)
-    if fixed_lyrics is None:
-        await ctx.send("Lyrics not found. Please check your spelling try again.")
-        return
-    await ctx.send("Interpreting lyrics...")
-    new_prompt = gpt_integration(fixed_lyrics)
-    if new_prompt is None:
-        await ctx.send("Something went wrong. Please try again.")
-        return
-    new_negative = None
-    new_style = None
-    new_size = "1344 768"
-    new_lora = None
-    includes_image = False
-    message = form_message(author_name, new_prompt, new_negative, new_style, new_size, new_lora, model_name)
-    try:
-        file_list = generate_image(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image)
-        await ctx.send(message, files=file_list)
-    except Exception as e:
-        print(e)
-        await ctx.send(ctx.author.mention + " Something went wrong. Please try again.")
-
-
-@bot.slash_command(description="Generate images based on song lyrics!")
-@option(
-    "song",
-    description="Enter the song name",
-    required=True
-)
-@option(
-    "artist",
-    description="Enter the artist name",
-    required=True
-)
-@option(
-    "model_name",
-    description="Enter the model name",
-    autocomplete=models_autocomplete,
-    required=False
-)
-async def music(ctx,
-                song: str,
-                artist: str,
-                model_name: str = None
-                ):
-    author_name = ctx.author.mention
-    await ctx.respond(
-        f"Generating images for {ctx.author.mention}\n**Song:** {song}\n**Artist:** {artist}")
-    fixed_lyrics = get_lyrics(song, artist)
-    if fixed_lyrics is None:
-        await ctx.send("Lyrics not found. Please check your spelling try again.")
-        return
-    await ctx.send("Got lyrics...")
-    lines = fixed_lyrics.split('\n')
-    lines = [line for line in lines if line.strip()]
-    lines = [line for line in lines if '[' not in line and ']' not in line]
-    random_lines = random.sample(lines, min(3, len(lines)))
-    output_line = ', '.join(random_lines)
-    new_prompt = song + ", " + output_line + ", " + artist
-    new_negative = None
-    new_style = None
-    new_size = None
-    new_lora = None
-    includes_image = False
-    message = form_message(author_name, new_prompt, new_negative, new_style, new_size, new_lora, model_name)
-    try:
-        file_list = generate_image(ctx, new_prompt, new_negative, new_style, new_size, new_lora, model_name, includes_image)
-        await ctx.send(message, files=file_list)
-    except Exception as e:
-        print(e)
-        await ctx.send(ctx.author.mention + " Something went wrong. Please try again.")
-
-
-@bot.slash_command(description='Generate an image using an image and words!')
-@option(
-    "attached_image",
-    description="Attach an image",
-    required=True
-)
-@option(
-    "new_prompt",
-    description="Enter the prompt",
-    required=True
-)
-@option(
-    "new_negative",
-    description="Enter things you don't want to see in the image",
-    required=False
-)
-@option(
-    "new_style",
-    description="Enter the style",
-    autocomplete=style_autocomplete,
-    required=False
-)
-@option(
-    "new_lora",
-    description="Choose the Lora model",
-    autocomplete=loras_autocomplete,
-    required=False
-)
-@option(
-    "model_name",
-    description="Enter the model name",
-    autocomplete=models_autocomplete,
-    required=False
-)
-async def redraw(ctx,
-                 attached_image: discord.Attachment,
-                 new_prompt: str,
-                 new_negative: str = None,
-                 new_style: str = None,
-                 new_lora: str = None,
-                 model_name: str = None
-                 ):
-    author_name = ctx.author.mention
-    await ctx.respond(f"Generating image for {ctx.author.mention}\n**Prompt:** {new_prompt}")
-    image_bytes = await attached_image.read()
-
-    # Process the image using PIL
-    image = Image.open(io.BytesIO(image_bytes))
-    width, height = image.size
-    aspect_ratio = height / width
-    print(f"Image aspect ratio: {aspect_ratio}")
-
-    closest_option = min(height_width_option, key=lambda option: abs(option["aspect_ratio"] - aspect_ratio))
-
-    new_width = closest_option["width"]
-    new_height = closest_option["height"]
-
-    new_img = image.resize((new_width, new_height), Image.LANCZOS)
-    print(f'New image size: {new_img.size}')
-    image.save(folder_path + '/input/temp_image.png')
-    output = io.BytesIO()
-    image.save(output, format='PNG')  # You can adjust the format if needed
-    output.seek(0)
-    await ctx.send(f"Original image:")
-    await ctx.send(file=discord.File(output, filename="original_image.png"))
-    print(f'New image saved to input/temp_image.png')
-    # convert height and width back to new_size string
-    new_size = str(new_height) + " " + str(new_width)
-    print(f'New size: {new_size}')
-    includes_image = True
-    message = form_message(author_name, new_prompt, new_negative, new_style, new_size, new_lora,
-                           model_name)
-    try:
-        file_list = generate_img2img(ctx, new_prompt, new_negative, new_style, new_size, new_lora,
-                                     model_name, includes_image)
-        await ctx.send(message)
-        await ctx.send("New image:", files=file_list)
-    except Exception as e:
-        print(e)
-        await ctx.send(ctx.author.mention + "img2img issue.")
 
 
 bot.run(TOKEN)
