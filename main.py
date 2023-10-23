6de3abe100cdc19c97b48732e5ab6ef443a39afa
AlexMKramer <93092632+AlexMKramer@users.noreply.github.com>
2023-10-23 15:23:45 -0600

    Test queue system

--- main.py
+++ main.py
@@ -515,6 +515,7 @@ async def draw(ctx,
              new_lora, lora_strength, artist_name, model_name))
     else:
         acknowledgment = await ctx.respond(f"On it!")
+        print(f'the acknowledgment is: {acknowledgment}')
         ack_id = acknowledgment.id
         print(f'the ack_id is: {ack_id}')
         await command_queue.put(
