7e4421e92b99315681e9f6df5f3472cb9d26f514
AlexMKramer <93092632+AlexMKramer@users.noreply.github.com>
2023-10-23 15:43:47 -0600

    Test queue system

--- main.py
+++ main.py
@@ -516,8 +516,7 @@ async def draw(ctx,
     else:
         acknowledgment = await ctx.respond(f"On it!")
         print(f'the acknowledgment id is: {acknowledgment.id}')
-        ack_id = int(acknowledgment.id)
-        print(f'the ack_id is: {ack_id}')
+        print(f'the acknowledgment id is: {acknowledgment.content}')
         await command_queue.put(
             (ctx.channel.id, author_name, message, acknowledgment.id, is_img2img, new_prompt, percent_of_original, new_negative, new_style, new_size,
              new_lora, lora_strength, artist_name, model_name))
